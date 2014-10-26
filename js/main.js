a0cea51621b67a0c3457f28a1d49e7d37b6e464b
PgLoLo <novgosh@gmail.com>
2014-10-26 22:46:36 +0300

    video speed changing added

--- js/main.js
+++ js/main.js
@@ -1,38 +1,44 @@
+function changeSpeed ()
+{
+    var videoNode = document.getElementById('player');
+    var speedSlider = document.getElementById('speedSlider');
+    var speedOutput = document.getElementById("speedOutput");
+
+    value = parseFloat(speedSlider.value);
+
+    speedOutput.value = value;
+    videoNode.playbackRate = value;
+}
+
+function test ()
+{
+    var videoNode = document.getElementById('player');
+    var testOutput = document.getElementById("testOutput");
+    testOutput.value = videoNode.currentTime;
+}
+
 (function localFileVideoPlayerInit(win) {
-    var URL = win.URL || win.webkitURL,
-        displayMessage = (function displayMessageInit() {
+    var URL = win.URL || win.webkitURL;
+    var displayMessage = (function displayMessageInit() {
             var node = document.querySelector('#message');
 
             return function displayMessage(message, isError) {
                 node.innerHTML = message;
                 node.className = isError ? 'error' : 'info';
             };
-        }()),
-
-        playSelectedFile = function playSelectedFileInit(event) {
+        }());
+    var playSelectedFile = function playSelectedFileInit(event) {
             var file = this.files[0];
-            var type = file.type;
             var videoNode = document.getElementById('player');
-            var canPlay = videoNode.canPlayType(type);
-
-            canPlay = (canPlay === '' ? 'no' : canPlay);
-
-            var message = 'Can play type "' + type + '": ' + canPlay;
-            var isError = canPlay === 'no';
-
-            displayMessage(message, isError);
-
-            if (isError) {
-                return;
-            }
-
+            var speedSlider = document.getElementById('speedSlider');
             var fileURL = URL.createObjectURL(file);
 
             videoNode.src = fileURL;
-            videoNode.playbackRate = 4.0;
-        },
+            videoNode.playbackRate = parseFloat(speedSlider.value);
+        };
 
-        inputNode = document.getElementById('fileInput');
+    var inputNode = document.getElementById('fileInput');
+    var videoNode = document.getElementById('player');
 
     if (!URL) {
         displayMessage('Your browser is not ' +
@@ -42,4 +48,5 @@
     }
 
     inputNode.addEventListener('change', playSelectedFile, false);
+    videoNode.addEventListener('timeupdate', test)
 }(window));
